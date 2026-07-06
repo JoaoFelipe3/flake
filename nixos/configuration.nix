0a19decaad49495d983397168a119bcb646f7a36
JoaoFelipe3 <ccristiany339@gmail.com>
2026-07-06 14:36:39 -0300

    feat: linux 7.1

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -146,6 +146,8 @@
     nushell
   ];
 
+  boot.kernelPackages = pkgs.linuxPackages_7_1;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
